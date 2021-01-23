8beb6ba102161ac5a4382e5b007a6f5fbdd92411
nibdo <hello@nibdo.com>
2021-01-23 19:51:48 +0100

    add device timezone

--- utils/datetimeParser.ts
+++ utils/datetimeParser.ts
@@ -3,8 +3,6 @@ import { DateTime } from 'luxon';
 //
 // Support for local datetime, timezones and floating times
 //
-
-const LOCAL_DATETIME: string = 'device'; // local timezone of user's device
 const FLOATING_DATETIME: string = 'floating'; // fixed datetime without timezone
 const UTC_TIMEZONE: string = 'UTC';
 
@@ -12,27 +10,19 @@ const UTC_TIMEZONE: string = 'UTC';
  * Parse datetime according different rules like local datetime, floating time and timezones
  * @param date
  * @param zone
- * @param toDateTime
+ * @param deviceTimezone
  * @constructor
  */
 export const DatetimeParser = (
   date: DateTime | string,
   zone: string,
+  deviceTimezone?: string
 ): string => {
   const dateString: string = typeof date === 'string' ? date : date.toString();
-  const thisDate: DateTime = DateTime.fromISO(dateString);
 
   const isFloatingDatetime: boolean = zone === FLOATING_DATETIME;
-  const isLocalDatetime: boolean = zone === LOCAL_DATETIME;
 
-  // No change as browser auto adjust current timezone
-  if (isLocalDatetime) {
-    return thisDate.toString();
-  }
-
-  // Adjust date with timezone so when converted to UTC it represents correct value
-  // Technically time will be not correct, but user wants it fixed
-  // Javascript will convert it to local timezone but with actual utc value
+  // Adjust date with timezone so when converted to UTC it represents correct value with fixed time
   if (isFloatingDatetime) {
     const dateFloating: DateTime = DateTime.fromISO(dateString, {
       zone: UTC_TIMEZONE,
@@ -41,33 +31,41 @@ export const DatetimeParser = (
     return dateFloating.toUTC().toISO();
   }
 
+  const thisDate: DateTime = DateTime.fromISO(dateString);
+
+  // Adjust datetime to device timezone
+  if (deviceTimezone) {
+    const dateConvert: DateTime = thisDate.setZone(zone);
+
+    return dateConvert.setZone(deviceTimezone).toString();
+  }
+
   return thisDate.toString();
 };
 
 export const parseToDateTime = (
     date: DateTime | string,
     zone: string,
+    deviceTimezone?: string,
 ): DateTime => {
   const dateString: string = typeof date === 'string' ? date : date.toString();
-  const thisDate: DateTime = DateTime.fromISO(dateString);
 
   const isFloatingDatetime: boolean = zone === FLOATING_DATETIME;
-  const isLocalDatetime: boolean = zone === LOCAL_DATETIME;
 
-  // No change as browser auto adjust current timezone
-  if (isLocalDatetime) {
-    return thisDate
-  }
-
-  // Adjust date with timezone so when converted to UTC it represents correct value
-  // Technically time will be not correct, but user wants it fixed
-  // Javascript will convert it to local timezone but with actual utc value
+  // Adjust date with timezone so when converted to UTC it represents correct value with fixed time
   if (isFloatingDatetime) {
     const dateFloating: DateTime = DateTime.fromISO(dateString, {
       zone: UTC_TIMEZONE,
     });
 
     return dateFloating.toUTC();
+  }
+
+  const thisDate: DateTime = DateTime.fromISO(dateString);
+
+  // Adjust datetime to device timezone
+  if (deviceTimezone) {
+    return thisDate.setZone(zone).setZone(deviceTimezone)
   }
 
   return thisDate;
